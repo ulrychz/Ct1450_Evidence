987fca65f1fea5ba179bb283940ea7c2e685ffc3
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-29 16:19:22 +0100

    Přidejte soubory projektu.

---
+++ Ct1450_Evidence/Models/Evidence.cs
@@ -0,0 +1,6 @@
+﻿namespace Ct1450_Evidence.Models
+{
+    public class Evidence
+    {
+    }
+}
---
+++ Ct1450_Evidence/Models/Polozka.cs
@@ -0,0 +1,23 @@
+﻿namespace Ct1450_Evidence.Models
+{
+    public class Polozka
+    {
+        public DateOnly Datum { get; set; }
+        private double naklady;
+
+        public double Naklady
+        {
+            get { return naklady; }
+            set { naklady = Math.Abs(value); }
+        }
+
+        private double vynosy;
+
+        public double Vynosy
+        {
+            get { return vynosy; }
+            set { vynosy = Math.Abs(value); }
+        }
+        public double Zisk => Vynosy - Naklady;
+    }
+}
